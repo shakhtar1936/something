84b860051129b49df1d3a71c2c19faf1a69f3af0
shakhtar1936 <109989800+shakhtar1936@users.noreply.github.com>
2022-09-09 03:29:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 from fastapi import FastAPI, Query
-import requests
+import httpx
+from schemas import Response_price, Response_trades
+from typing import List
 
 app = FastAPI()
 
-base = 'https://fapi.binance.com'
-path1 = '/fapi/v1/ticker/price'
-url1 = base+path1
-path2 = '/fapi/v1/trades'
-url2 = base+path2
+BASE_API_URL = 'https://fapi.binance.com'
 
-@app.get('/api/v1/ticker/price/{symbol}')
+@app.get('/api/v1/ticker/price/{symbol}', response_model=Response_price)
 def get_ticker_price(symbol: str):
     param = {'symbol': symbol}
-    r = requests.get(url1, params=param)
+    r = httpx.get(f'{BASE_API_URL}/fapi/v1/ticker/price', params=param)
     if r.status_code == 200:
         return r.json()
     else:
         return {'key': 'error'}
 
-@app.get('/api/v1/trades/{symbol}')
+@app.get('/api/v1/trades/{symbol}', response_model=List[Response_trades])
 def get_trades(symbol: str, limit: int = Query(None, limit=1000)):
     param = {'symbol': symbol, 'limit': limit}
-    r = requests.get(url2, params=param)
+    r = httpx.get(f'{BASE_API_URL}/fapi/v1/trades', params=param)
     if r.status_code == 200:
         return r.json()
     else:
