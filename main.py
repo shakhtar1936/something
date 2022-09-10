5499e0c78a690f7023a46cf89e6a7a77d2958ba1
shakhtar1936 <109989800+shakhtar1936@users.noreply.github.com>
2022-09-11 01:55:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,20 @@ app = FastAPI()
 BASE_API_URL = 'https://fapi.binance.com'
 
 @app.get('/api/v1/ticker/price/{symbol}', response_model=Response_price)
-def get_ticker_price(symbol: str):
+async def get_ticker_price(symbol: str):
     param = {'symbol': symbol}
-    r = httpx.get(f'{BASE_API_URL}/fapi/v1/ticker/price', params=param)
+    async with httpx.AsyncClient() as client:
+        r = await client.get(f'{BASE_API_URL}/fapi/v1/ticker/price', params=param)
     if r.status_code == 200:
         return r.json()
     else:
         return {'key': 'error'}
 
 @app.get('/api/v1/trades/{symbol}', response_model=List[Response_trades])
-def get_trades(symbol: str, limit: int = Query(None, limit=1000)):
+async def get_trades(symbol: str, limit: int = Query(None, limit=1000)):
     param = {'symbol': symbol, 'limit': limit}
-    r = httpx.get(f'{BASE_API_URL}/fapi/v1/trades', params=param)
+    async with httpx.AsyncClient() as client:
+        r = httpx.get(f'{BASE_API_URL}/fapi/v1/trades', params=param)
     if r.status_code == 200:
         return r.json()
     else:
